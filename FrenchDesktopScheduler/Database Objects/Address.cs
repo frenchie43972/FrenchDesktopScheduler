3b905d3ded350c6e21fce50df746622ff9afb735
Kris French <frenchie43972@gmail.com>
2021-11-22 04:06:12 +0000

    Added DB Object

---
+++ FrenchDesktopScheduler/Database Objects/Address.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrenchDesktopScheduler.Database_Objects
+{
+	class Address
+	{
+        public int AddressId { get; set; }
+        public string AddressLine { get; set; }
+        public string AddressLine2 { get; set; }
+        public int CityId { get; set; }
+        public string PostalCode { get; set; }
+        public string Phone { get; set; }
+        public DateTime CreateDate { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime LastUpdate { get; set; }
+        public string LastUpdateBy { get; set; }
+
+        private static int count = 0;
+
+
+        public Address(int addressId, string addressLine, string addressLine2, int cityId, string postalCode, string phone, DateTime createDate, string createdBy, DateTime lastUpdate, string lastUpdateBy)
+        {
+            AddressId = addressId;
+            if (addressId > count)
+            {
+                count = addressId;
+            }
+            AddressLine = addressLine;
+            AddressLine2 = addressLine2;
+            CityId = cityId;
+            PostalCode = postalCode;
+            Phone = phone;
+            CreateDate = createDate;
+            CreatedBy = createdBy;
+            LastUpdate = lastUpdate;
+            LastUpdateBy = lastUpdateBy;
+        }
+    }
+}
